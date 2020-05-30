248d300b0e51bff01495e7cfd8460f208cbbe123
gabriel-severo <gabriel.ivi@hotmail.com>
2020-05-30 09:58:33 -0400

    Add express and nodemon

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const server = express()
+
+server.use(express.static("public"))
+
+server.get('/', function(req, res){
+    return res.sendFile(__dirname + "/index.html")
+})
+
+server.listen(3000)
